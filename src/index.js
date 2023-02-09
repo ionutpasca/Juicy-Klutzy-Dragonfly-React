bb9c0625ea4a0e109a17ae6a4df8960da4e33763
Pașca Ionuț <pascaionut94@gmail.com>
2023-02-09 16:34:55 +0200

    Commit from TeleportHQ

--- src/index.js
+++ src/index.js
@@ -11,8 +11,6 @@ const App = () => {
     <Router>
       <div>
         <Route component={Home} exact path="/" />
-      <div> Servus, lume </div>
-        <Route component={Home} exact path="/" />
         <Route component={Page} exact path="/page" />
       </div>
     </Router>
